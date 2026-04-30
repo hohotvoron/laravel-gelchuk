57d64c3495f8d7e8bbf1909d4a70c83b1e3223d0
mrregari <gameplusone9@gmail.com>
2026-04-30 09:27:44 +0500

    гельчук практика 3-4

--- app/Http/Controllers/Admin/MainController.php
+++ app/Http/Controllers/Admin/MainController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index(){
+        // $tag = new Tag();
+        // $tag->title='Привет beer!';
+        // $tag->save();
         return view('admin.index');
     }
 }
