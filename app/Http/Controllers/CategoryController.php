157c25d75b0233e34647484d7a077b30ce2b4411
k3lar0nie <k3lar0nie@yahoo.com>
2026-05-18 11:57:21 +0500

    практика 16 гельчук

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function show($slug){
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $posts = $category->posts()->orderBy('id', 'desc')->paginate(2);
+        return view('user.posts.categories', compact('category', 'posts'));
+    }
 }
--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Tag;
 
 class TagController extends Controller
 {
-    //
+    public function show($slug){
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+        $posts = $tag->posts()->with('category')->orderBy('id', 'desc')->paginate(2);
+        return view('user.posts.tags', compact('tag', 'posts'));
+    }
 }
